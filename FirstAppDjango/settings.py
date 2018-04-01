f79820b9553bb17317fc2786489880437e80b4e0
Ubuntu <spitslide@gmail.com>
2018-04-01 23:23:32 +0200

    comment out import

--- FirstAppDjango/settings.py
+++ FirstAppDjango/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from secrets import KEY, DATABASE_USER, DATABASE_PASSWORD
+# from secrets import KEY, DATABASE_USER, DATABASE_PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
