ded8f1a14e0c9764adc7a554d0dbfa1d7b0ae2ab
Ubuntu <spitslide@gmail.com>
2018-04-01 23:44:32 +0200

    added allowed hosts

--- FirstAppDjango/settings.py
+++ FirstAppDjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['linksbookmarks.herokuapp.com']
 
 
 # Application definition
