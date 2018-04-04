d38efb006f1771e2ec02fc354ea6bddf33a05044
Ubuntu <spitslide@gmail.com>
2018-04-04 20:41:00 +0200

    database

--- FirstAppDjango/settings.py
+++ FirstAppDjango/settings.py
@@ -79,17 +79,17 @@ import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'notesdatabase',
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD' : os.environ.get('DATABASE_PASSWORD'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         # 'ENGINE': 'django.db.backends.mysql',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'notesdatabase',
+#         'USER': os.environ.get('DATABASE_USER'),
+#         'PASSWORD' : os.environ.get('DATABASE_PASSWORD'),
+#     }
+# }
 
 
 # Password validation
